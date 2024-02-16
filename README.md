3b3be81e5da05d3cec0a30602d522979269868d3
ylyu <yl.yu@pulmuone.com>
2024-02-16 21:59:54 +0900

    plir

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ https://www.figma.com/file/wDX6ctFyIztUPq0cU8HqZ0/VAN-%EC%85%80%ED%94%84%EC%98%A
 01 매장리스트:매장선택 - <a href="https://yl1017-yang.github.io/mo-van/pages/store_select.html" target="_blank">https://yl1017-yang.github.io/mo-van/pages/store_select.html</a>
 
 01 매장리스트:지도보기 - <a href="https://yl1017-yang.github.io/mo-van/pages/store_map.html" target="_blank">https://yl1017-yang.github.io/mo-van/pages/store_map.html</a>
+
+02 상품상세 - <a href="https://yl1017-yang.github.io/mo-van/pages/store_detail.html" target="_blank">https://yl1017-yang.github.io/mo-van/pages/store_detail.html</a>
--- resources/js/common.js
+++ resources/js/common.js
@@ -1,3 +1,4 @@
+
 $(function () {
 	$(window).on('load', function () {
 		sliderVisual();
@@ -44,6 +45,64 @@ $(function () {
 		$("#" + $(this).data('id')).addClass('on');
 	});
 });
+
+//상품상세 수량계산
+let basket = {
+	totalCount: 0,
+	totalPrice: 0, 
+	
+	//재계산
+	reCalc: function() {
+	this.totalCount = 0;
+	this.totalPrice = 10000;  //필수가격
+	document.querySelectorAll(".p_num").forEach(function(item) {
+		/* if (item.parentElement.parentElement.parentElement.previousElementSibling.firstElementChild.firstElementChild.checked == true) { */
+		var count = parseInt(item.getAttribute('value'));
+		this.totalCount += count;
+		var price = item.parentElement.previousElementSibling.firstElementChild.getAttribute('value');
+		this.totalPrice += count * price;
+		/* } */
+	}, this);
+	},
+	
+	//화면 업데이트
+	updateUI: function() {
+	document.querySelector('#sum_p_price').textContent = this.totalPrice.formatNumber();
+	},
+	
+	//개별 수량 변경
+	changePNum: function(pos) {
+	var item = document.querySelector('input[name=p_num' + pos + ']');
+	var p_num = parseInt(item.getAttribute('value'));
+	var newval = event.target.classList.contains('up') ? p_num + 1 : event.target.classList.contains('down') ? p_num - 1 : event.target.value;
+
+	if (parseInt(newval) < 0 || parseInt(newval) > 99) {
+		return false;
+	}
+
+	item.setAttribute('value', newval);
+	item.value = newval;
+
+	var price = item.parentElement.previousElementSibling.firstElementChild.getAttribute('value');
+	item.parentElement.nextElementSibling.textContent = (newval * price).formatNumber() + "원";
+
+	this.reCalc();
+	this.updateUI();
+	},
+	// checkItem: function() {
+	//   this.reCalc();
+	//   this.updateUI();
+	// },  
+}
+
+// 숫자 3자리 콤마찍기
+Number.prototype.formatNumber = function() {
+	if (this == 0) return 0;
+	let regex = /(^[+-]?\d+)(\d{3})/;
+	let nstr = (this + '');
+	while (regex.test(nstr)) nstr = nstr.replace(regex, '$1' + ',' + '$2');
+	return nstr;
+};
 
 
 //바텀시트 핸들바
